425b3d7baf3c3f325e29818a073693267a0a783a
Kelsey Langlois <kels.langlois@gmail.com>
2022-01-11 15:02:57 -0800

    Choose random NPC after interaction.

--- Assets/Scripts/Characters.cs
+++ Assets/Scripts/Characters.cs
@@ -7,8 +7,16 @@ public class Characters : MonoBehaviour
 
     public delegate int Decision();
     public static event System.Action<NPC> UpdateNPC;
-    public NPC activeNPC = new NPC("Cat", 10, 5, 0, () => Random.Range(0,3));
     bool npcUpdated = false;
+
+    List<NPC> npcs = new List<NPC> {
+        new NPC("Cat", 10, 5, 0, () => Random.Range(0,3)),
+        new NPC("Slime", 1, 1, 0, () => 0),
+        new NPC("Spider", 5, 5, 0, () => Random.Range(0,2)),
+        new NPC("Wolf", 10, 10, 0, () => Random.Range(0,3)),
+        new NPC("Crow", 10, 10, 0, () => 1)
+    };
+    int activeNPCIndex = Random.Range(0, 5);
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +28,7 @@ public class Characters : MonoBehaviour
     void Update()
     {
         if (!npcUpdated && UpdateNPC != null) {
-            UpdateNPC(activeNPC);
+            UpdateNPC(GetActiveNPC());
             npcUpdated = true;
         }
     }
@@ -29,13 +37,18 @@ public class Characters : MonoBehaviour
         Board.NPCConsequences -= SaveConsequences;
     }
 
+    void GetNewNPC() {
+        activeNPCIndex = Random.Range(0, npcs.Count);
+    }
+
     public void SaveConsequences(int deltaHealth, int deltaGold) {
-        activeNPC.SaveConsequences(deltaHealth, deltaGold);
-        if (UpdateNPC != null) UpdateNPC(activeNPC);
+        GetActiveNPC().SaveConsequences(deltaHealth, deltaGold);
+        GetNewNPC();
+        if (UpdateNPC != null) UpdateNPC(GetActiveNPC());
     }
 
     public NPC GetActiveNPC() {
-        return activeNPC;
+        return npcs[activeNPCIndex];
     }
 
     public struct NPC {
